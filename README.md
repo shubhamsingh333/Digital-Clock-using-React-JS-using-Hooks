776a44b1ea83f15e55f906e598fdebfef31abb47
shubhamsingh333 <shubhamsingh333333@gmail.com>
2021-05-02 20:28:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-s5l3iv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-s5l3iv)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./App";
+
+const App = () => {
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(time);
+  const UpdateTime = () => {
+    time =new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+  setInterval(UpdateTime,1000);
+
+  return (
+    <>
+      <h1> {ctime} </h1>
+      {/* <button onClick={UpdateTime}>get time</button> */}
+    </>
+  );
+};
+
+export default App;
